79777a141a933b1fc4ac1b01ac2418dcbc5d3d73
jordi1964 <41989049+jordi1964@users.noreply.github.com>
2019-02-14 19:15:39 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*  Proves NO GIRAR MOBIL-3
+========================================== */
+
+
+/** Script detecció rotació **/
+function openTongue() {
+  document.getElementById('brrt').style.display = "block";
+}
